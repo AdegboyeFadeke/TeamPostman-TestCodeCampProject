c9802d65b5ead695289badcf8d1a128ad82fa9e3
Fadeke <fadegboye59@gmail.com>
2023-03-16 08:18:29 +0100

    Added Testcodecamp assignment,task 14

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world, this is my first java projects");
+        System.out.println("I would like to introduce myself");
+        System.out.println("My name is Fadeke and I love the experience of learning java");
+        System.out.println("Hello testCode, this is my second line of code");
+        System.out.println("This is my experiment to print out words in the console");
+        System.out.println("My cat looks like this a pedesteran lane");
+        System.out.println("-------/////////-----------");
+        System.out.println("---/////////////////---");
+        System.out.println("---/////////////////---");
+        System.out.println("-------////////--------");
+    }
+}
