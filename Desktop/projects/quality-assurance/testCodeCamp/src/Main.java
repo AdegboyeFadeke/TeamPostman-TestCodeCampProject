4f00c9657586a7bef3960e5243cb949506f3d76f
Fadeke <fadegboye59@gmail.com>
2023-03-17 02:52:50 +0100

    Added task 14

---
+++ Desktop/projects/quality-assurance/testCodeCamp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first project with Java and IntelliJ IDE.");
+        System.out.println("I am happy to have come this far as a tester.");
+        System.out.println("I would like to thank everyone who has brought me this far;");
+        System.out.println("My teachers, My Ap, My leader and My assistant group leader.");
+        System.out.println("Also, everyone who is in Team-postman. I love you all.");
+        System.out.println("From Testify to the world.... We leave no man behind.");
+    }
+}
